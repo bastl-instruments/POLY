332bf56be9723ec6d56c75a297432fb6123614ed
standuino <standuino@gmail.com>
2013-11-01 21:05:46 +0100

    added Test Mode - couple of other changes

--- POLY.ino
+++ POLY.ino
@@ -1,4 +1,4 @@
- // libraries
+// libraries
 #include <MIDI.h>
 #include <EEPROM.h>
 #include <avr/pgmspace.h>
@@ -13,21 +13,20 @@
 #include <lfo.h>
 #include <Oscil.h>
 
-#include <tables/sin2048_int8.h>
-#include <tables/saw2048_int8.h> 
+#include <tables/sin1024_int8.h>
+#include <tables/saw1024_int8.h> 
 #include <tables/square_analogue512_int8.h> 
-#include <bastlTables/whitenoise2048_int8.h>
+//#include <bastlTables/whitenoise2048_int8.h>
 
 //#include <tables/whitenoise2048_int8.h>
 //#include <tables/square_no_alias512_int8.h>
 //#include <python/hat.h>
 unsigned char inputChannel;
+//SQUARE_ANALOGUE512_DATA
+const char* WAVE_TABLES[]={ //SQUARE_ANALOGUE512_DATA
+  SIN1024_DATA, SAW1024_DATA, SQUARE_ANALOGUE512_DATA};//SQUARE_NO_ALIAS512_DATA};//HAT_DATA};//WHITENOISE2048_DATA};
 
-const char* WAVE_TABLES[]={
-  SIN2048_DATA, SAW2048_DATA, SQUARE_ANALOGUE512_DATA,WHITENOISE2048_DATA};//SQUARE_NO_ALIAS512_DATA};//HAT_DATA};//WHITENOISE2048_DATA};
-
-#define NUMBER_OF_WAVETABLES 4
-
+#define NUMBER_OF_WAVETABLES 3
 #define NUMBER_OF_VOICES 3
 
 #define CONTROL_RATE 64 // powers of 2 please
@@ -37,20 +36,22 @@ Oscil<SIN2048_NUM_CELLS, AUDIO_RATE> aOsc[NUMBER_OF_VOICES]={
  Oscil<SIN2048_NUM_CELLS, AUDIO_RATE> aMod[NUMBER_OF_VOICES]={
  (SIN2048_DATA)};
  */
-Oscil<SIN2048_NUM_CELLS, AUDIO_RATE> aOsc[NUMBER_OF_VOICES]={
-  (SIN2048_DATA),(SIN2048_DATA),(SIN2048_DATA)};
-Oscil<SIN2048_NUM_CELLS, AUDIO_RATE> aMod[NUMBER_OF_VOICES]={
-  (SIN2048_DATA),(SIN2048_DATA),(SIN2048_DATA)};
+Oscil<SIN1024_NUM_CELLS, AUDIO_RATE> aOsc[NUMBER_OF_VOICES]={
+  (SIN1024_DATA),(SIN1024_DATA),(SIN1024_DATA)};
+Oscil<SIN1024_NUM_CELLS, AUDIO_RATE> aMod[NUMBER_OF_VOICES]={
+  (SIN1024_DATA),(SIN1024_DATA),(SIN1024_DATA)};
 
 envelope ADSR[NUMBER_OF_VOICES];
 lfo LFO[NUMBER_OF_VOICES];
 StateVariable lpf[NUMBER_OF_VOICES];
 
 trinityHW hw; // MOZZI or DEFAULT
-
+boolean test=false;
+boolean pokemon=false;
 void setup() {
-  
-  
+test=false;
+pokemon=false;
+
   // debug();
   hw.setFreezeType(UNFREEZE_EXTERNALY);
   hw.initialize(DEFAULT);
@@ -59,13 +60,17 @@ void setup() {
   //for(int i=0;i<1024;i++) EEPROM.write(i,0);
   animation();
   initMem();
-  
+
   // setAllTables();
   initMidi(getMidiChannel());
   startMozzi(CONTROL_RATE);
   hw.update();
   hw.update();
+  hw.update();
+  hw.update();
   checkForPokemon();
+  hw.update();
+  hw.update();
 }
 
 void updateControl(){
@@ -82,6 +87,8 @@ void updateControl(){
 void loop() {
   audioHook();
 }
+
+
 
 
 
